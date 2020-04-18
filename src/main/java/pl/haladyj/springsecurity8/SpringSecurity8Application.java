f1bbee1270234ee208720208015077ab70a2800a
Pawel Haladyj <pawel@haladyj.pl>
2020-04-18 13:27:06 +0200

    initial commit

---
+++ src/main/java/pl/haladyj/springsecurity8/SpringSecurity8Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity8Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/springsecurity8/SpringSecurity8ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
